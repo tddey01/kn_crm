ecead3a1267cd9e3ec749e7de8d75ccb46972806
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-09-24 20:51:38 +0800

    定义添加用户显示规则

--- web/stark.py
+++ web/stark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-from stark.service.v1 import site, StarkHandler
+from stark.service.v1 import site, StarkHandler,get_choice_text,StarkModelForm
 from web import models
 
 
@@ -8,5 +8,25 @@ class SchoolHandler(StarkHandler):
     # list_display = ['title',StarkHandler.display_edit,StarkHandler.display_del]
     list_display = ['title',]
 
-
 site.register(models.School, SchoolHandler)
+
+
+class DepartmentHandler(StarkHandler):
+    list_display = ['title',]
+
+
+site.register(models.Department,DepartmentHandler)
+
+
+class UserInfoModelForm(StarkModelForm):
+    class Meta:
+        model = models.UserInfo
+        fields = ['name','password','nickname','gender','phone','email','depart','roles']
+
+
+
+class UserInfoHandler(StarkHandler):
+    list_display = ['nickname',get_choice_text('性别','gender'),'phone','email','depart']
+    model_form_class = UserInfoModelForm
+
+site.register(models.UserInfo,UserInfoHandler)
