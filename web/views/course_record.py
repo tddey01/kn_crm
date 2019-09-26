723b254c4d97b9e553f3c65ccf018b5c64a33f0a
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-09-26 15:39:31 +0800

    crm 开发之 新增批量生成考勤记录

--- web/views/course_record.py
+++ web/views/course_record.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from django.conf.urls import url
+from django.shortcuts import HttpResponse
 from django.utils.safestring import mark_safe
 from stark.service.v1 import StarkHandler, StarkModelForm, get_datetime_text
 from web import models
@@ -14,7 +15,9 @@ class CourseRecordModelForm(StarkModelForm):
 
 class CourseRecordHandler(StarkHandler):
     model_form_class = CourseRecordModelForm
-    list_display = ['class_object', 'day_num', 'teacher', get_datetime_text('时间', 'date')]
+
+    list_display = [StarkHandler.display_checkbox, 'class_object', 'day_num', 'teacher',
+                    get_datetime_text('时间', 'date')]
 
     def display_edit_del(self, obj=None, is_header=None, *args, **kwargs):
         if is_header:
@@ -47,3 +50,32 @@ class CourseRecordHandler(StarkHandler):
         if not is_update:
             form.instance.class_object_id = class_id
         form.save()
+
+    def action_multi_init(self, request, *args, **kwargs):
+        course_record_id_list = request.POST.getlist('pk')
+        class_id = kwargs.get('class_id')
+        class_object = models.ClassList.objects.filter(id=class_id).first()
+        if not class_object:
+            return HttpResponse('班级不存在')
+        student_object_list = class_object.student_set.all()
+
+        for course_record_id in course_record_id_list:
+            # 判断上课记录是否合法
+            course_record_object = models.CourseRecord.objects.filter(id=course_record_id,
+                                                                      class_object_id=class_id).first()
+            if not course_record_object:
+                continue
+
+            # 判断此上课记录的考勤记录是否已经存在
+            study_record_exists = models.StudyRecord.objects.filter(course_record=course_record_object).exists()
+            if study_record_exists:
+                continue
+
+            # 为每个学生在该天创建考勤记录
+            study_record_object_list = [models.StudyRecord(student_id=stu.id, course_record_id=course_record_id) for stu
+                                        in student_object_list]
+
+            models.StudyRecord.objects.bulk_create(study_record_object_list, batch_size=50)
+
+    action_multi_init.text = '批量初始化考勤'
+    action_list = [action_multi_init, ]
